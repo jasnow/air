7682e5b05a3303cd6225196c6738241944ffda18
Al Snow <jasnow@hotmail.com>
2014-05-26 20:28:30 -0400

    Added linux logic to open cat gif; fixed typo

--- app/models/cat_request.rb
+++ app/models/cat_request.rb
@@ -5,6 +5,10 @@ class CatRequest < ActiveRecord::Base
   def fulfill
     sleep 5 # Pretend this is harder than it really is
     update_attributes cat: Cat.choose
-    `open #{cat.download_path} -a 'Google Chrome'`
+    if RUBY_PLATFORM == "i686-linux"
+      `gnome-open #{cat.download_path}`
+    else
+      `open #{cat.download_path} -a 'Google Chrome'`
+    end
   end
 end
